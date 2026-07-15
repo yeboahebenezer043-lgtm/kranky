7de1a6b8269f8c23fde797e710c230d91ce0cb42
Kranky hub <yeboahebenezer043@gmail.com>
2026-07-15 15:32:18 +0000

    Create app.js

---
+++ app.js
@@ -0,0 +1,19 @@
+function goToBuy(){
+
+    window.location = "buy-data.html";
+
+}
+
+
+function goToLogin(){
+
+    window.location = "login.html";
+
+}
+
+
+function showMessage(){
+
+    alert("Welcome to Kranky Data Services");
+
+}
